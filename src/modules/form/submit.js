83576bac66a51f4dbf160d421e9b245830ae980c
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2024-11-08 23:20:22 -0300

    Preenchimento e validação de formulario

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,7 +1,9 @@
 import dayjs from "dayjs"
+import { hoursClick } from "./hours-click"
 
 const form = document.querySelector("form")
 const selectedDate = document.getElementById("date")
+const clientName = document.getElementById("client")
 
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 
@@ -10,5 +12,32 @@ selectedDate.min = inputToday
 
 form.onsubmit = (e) => {
 	e.preventDefault();
-	console.log("Enviando");
+	try {
+		const name = clientName.value.trim()
+
+		if (!name) {
+			return alert("Informe o nome do cliente")
+		}
+
+		const hourSelected = document.querySelector(".hour-selected")
+
+		if (!hourSelected) {
+			return alert("Selecione o horário")
+		}
+
+		const [hour] = hourSelected.innerText.split(":")
+
+		const when = dayjs(selectedDate.value).add(hour, "hour")
+
+		const id = new Date.getTime()
+
+		console.log({id, name, hourSelected});
+		
+	} catch (error) {
+		alert("Não foi possivel realizar o agendamento")
+		console.log(error
+
+		);
+
+	}
 }
